3d12be855eee837941ef4d627c69f71fb3377904
Slawek Paciorek <slawekpaciorek@gmail.com>
2018-09-18 16:54:01 +0200

    add some sliding function

--- assets/function.js
+++ assets/function.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     let $portfolioDiv = $(".project>div");
-    let $portfolioP = $(".project").find("div").children();
+    let $portfolioP = $(".project").find("div>p");
 
     $portfolioP.hide();
 
@@ -9,5 +9,23 @@ $(document).ready(function(){
         $(this).children().toggle();
     });
 
-
+    sliding();
 });
+
+function sliding(){
+    let sliderElements = [$("#1"),$("#2"),$("#3"),$("#4")];
+    let n=0;
+    $('#left').on('click',function(){
+        sliderElements[n].hide();
+        sliderElements[n].next().show();
+        n++;
+    });
+
+    $('#right').on('click', function(){
+       sliderElements[n].hide();
+       sliderElements[n].prev().show();
+        n--;
+    });
+
+
+}
